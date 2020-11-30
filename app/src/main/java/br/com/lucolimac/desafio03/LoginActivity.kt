eea345eb527f2fe75835629d310802df55b5fe90
Lucas de Oliveira Macedo <lucasomac@outlook.com>
2020-11-30 08:54:49 -0300

    [LOGIN] Finalizado o layout. Adicionado o viewBinding na tela. Faltando navegação.

--- app/src/main/java/br/com/lucolimac/desafio03/LoginActivity.kt
+++ app/src/main/java/br/com/lucolimac/desafio03/LoginActivity.kt
@@ -2,16 +2,14 @@ package br.com.lucolimac.desafio03
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import br.com.lucolimac.desafio03.databinding.ActivityLoginBinding
 
 
 class LoginActivity : AppCompatActivity() {
-//    private lateinit var binding: ResultProfileBinding
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
-//        binding = ResultProfileBinding.inflate(layoutInflater)
-//        val view = binding.root
-//        setContentView(view)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
-
 }
