ad83a7c5753a1c90bc79f0935de6a7e3350c0a7a
Lucas de Oliveira Macedo <lucasomac@outlook.com>
2020-11-29 23:28:32 -0300

    TEla Splash

---
+++ app/src/main/java/br/com/lucolimac/desafio03/LoginActivity.kt
@@ -0,0 +1,17 @@
+package br.com.lucolimac.desafio03
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+class LoginActivity : AppCompatActivity() {
+//    private lateinit var binding: ResultProfileBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+//        binding = ResultProfileBinding.inflate(layoutInflater)
+//        val view = binding.root
+//        setContentView(view)
+    }
+
+}
