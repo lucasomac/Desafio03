2af5991d0e09f7772e1105334ed0f3f35519034f
Lucas de Oliveira Macedo <lucasomac@outlook.com>
2020-11-30 11:32:00 -0300

    [REGISTER] Finalizado o layout. Adicionado o viewBinding na tela. Faltando navegação.

---
+++ app/src/main/java/br/com/lucolimac/desafio03/RegisterActivity.kt
@@ -0,0 +1,14 @@
+package br.com.lucolimac.desafio03
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import br.com.lucolimac.desafio03.databinding.ActivityRegisterBinding
+
+class RegisterActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRegisterBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
--- app/src/main/java/br/com/lucolimac/desafio03/SplashActivity.kt
+++ app/src/main/java/br/com/lucolimac/desafio03/SplashActivity.kt
@@ -5,11 +5,14 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
+import br.com.lucolimac.desafio03.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         Handler(Looper.getMainLooper()).postDelayed({
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
