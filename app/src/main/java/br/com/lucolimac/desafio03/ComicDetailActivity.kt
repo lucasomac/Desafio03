48d8422404483eac55db9a4d7279c5f9b9a73bf0
Lucas de Oliveira Macedo <lucasomac@outlook.com>
2020-12-02 22:45:16 -0300

    [DETAIL] Criação do layout inicial da activity de detail

---
+++ app/src/main/java/br/com/lucolimac/desafio03/ComicDetailActivity.kt
@@ -0,0 +1,11 @@
+package br.com.lucolimac.desafio03
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ComicDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_comic_detail)
+    }
+}
